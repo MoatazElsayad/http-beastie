f1c178bd03e248c02dc1501a8faf4d82d01ec057
Moataz Badawy <zezomostafa130@gmail.com>
2026-05-07 14:03:08 +0300

    Serve static index.html for GET / requests

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <string>
+
 #include <boost/beast/http.hpp>
 
 boost::beast::http::response<boost::beast::http::string_body> handle_request(const boost::beast::http::request<boost::beast::http::string_body>& req);
+
+std::string read_file(const std::string& file_path);
--- src/utils.cpp
+++ src/utils.cpp
@@ -14,8 +14,13 @@ http::response<http::string_body> handle_request(const http::request<http::strin
 
   if (req.method() == http::verb::get) {
     if (req.target() == "/") {
-      res.result(http::status::ok);
-      res.body() = "<h1 style=\"text-align: center;\">CSCE 1102</h1>";
+      try {
+        res.result(http::status::ok);
+        res.body() = read_file("static/index.html");
+      } catch (...) {
+        res.result(http::status::unavailable);
+        res.body() = "<h1 style=\"text-align: center;\">Failed to load index</h1>";
+      }
     } else {
       res.result(http::status::not_found);
       res.body() = "<h1 style=\"text-align: center;\">404 Not Found</h1>";
@@ -28,4 +33,13 @@ http::response<http::string_body> handle_request(const http::request<http::strin
 
   res.prepare_payload();
   return res;
+}
+
+std::string read_file(const std::string& file_path) {
+    std::ifstream file(file_path, std::ios::binary);
+    if (!file.is_open())
+        throw std::runtime_error("Failed to open file: " + file_path);
+    std::ostringstream buffer;
+    buffer << file.rdbuf();
+    return buffer.str();
 }
